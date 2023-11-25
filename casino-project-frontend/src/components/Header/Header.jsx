ef5b9c1069feacdab2765a60052e0029e51702b0
Daniel <daniilbondarcov@gmail.com>
2023-11-26 01:31:07 +0300

    implement logout

--- casino-project-frontend/src/components/Header/Header.jsx
+++ casino-project-frontend/src/components/Header/Header.jsx
@@ -1,65 +1,77 @@
 import React from "react";
 import Back from "./assets/banner.png";
 import "./index.css";
+import {useNavigate} from "react-router-dom";
+import {useDispatch} from "react-redux";
+import {setUser} from "../../app/features/userSlice";
 
 export const Header = () => {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: 200,
-        marginBottom: 20,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <div
-        style={{
-          backgroundImage: `url(${Back})`,
-          height: 100,
-          width: "100%",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      />
-      <div
-        style={{
-          height: 100,
-          width: "100%",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "space-between",
-          paddingLeft: 20,
-          paddingRight: 20,
-        }}
-      >
-        <h1
-          style={{
-            fontSize: 32,
-            color: "#fff",
-            fontWeight: "bold",
-            textTransform: "uppercase",
-          }}
-        >
-          Casino
-        </h1>
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const logout = () => {
+        dispatch(setUser(null))
+        localStorage.removeItem('user')
+        navigate("/")
+    }
+
+    return (
         <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            minWidth: 450,
-            justifyContent: "space-between",
-          }}
+            style={{
+                width: "100%",
+                height: 200,
+                marginBottom: 20,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "space-between",
+            }}
         >
-          <p style={{ color: "#fff", fontSize: 24 }}>Balance</p>
-          <p style={{ color: "gold", fontSize: 24 }}>$ 999.99</p>
-          <button className="button-header">Выйти</button>
+            <div
+                style={{
+                    backgroundImage: `url(${Back})`,
+                    height: 100,
+                    width: "100%",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundRepeat: "no-repeat",
+                }}
+            />
+            <div
+                style={{
+                    height: 100,
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                }}
+            >
+                <h1
+                    style={{
+                        fontSize: 32,
+                        color: "#fff",
+                        fontWeight: "bold",
+                        textTransform: "uppercase",
+                    }}
+                >
+                    Casino
+                </h1>
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        minWidth: 450,
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <p style={{color: "#fff", fontSize: 24}}>Balance</p>
+                    <p style={{color: "gold", fontSize: 24}}>$ 999.99</p>
+                    <button className="button-header" onClick={logout}>Выйти</button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
